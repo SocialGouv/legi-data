7f6f1af4e97d31a520663604c8444b4223580b87
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2019-12-20 09:58:50 +0100

    fix: increase concurrency

--- src/getCode.js
+++ src/getCode.js
@@ -59,7 +59,7 @@ const embedArticles = async section => ({
           debug(`embedArticles section ${section.id}`);
           return embedArticles(section);
         }),
-      { concurrency: 1 }
+      { concurrency: 2 }
     ))
 });
 
