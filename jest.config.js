c2fc3ee59bc628f91f0416ce95aefbcf9e4a9700
Douglas Duteil <douglasduteil@gmail.com>
2020-11-04 18:18:10 +0100

    chore(jest): do no coverage and verbose by default

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,8 @@
 module.exports = {
   bail: true,
-  collectCoverage: true,
   collectCoverageFrom: ["<rootDir>/src/**/*.js", "<rootDir>/scripts/**/*.js"],
   coverageDirectory: "<rootDir>/coverage",
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
   },
-  verbose: true,
 };
