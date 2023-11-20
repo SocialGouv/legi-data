11adc24eb7b1d53493ae40e529e71545fb06930c
Victor <victor.zeinstra@gmail.com>
2023-11-20 15:48:35 +0100

    fix: legifrance-beta -> legifrance

--- scripts/setup.js
+++ scripts/setup.js
@@ -19,7 +19,7 @@ prompt.message = "";
 prompt.start();
 prompt.get(SCHEMA, function (err, result) {
   const source = [
-    `API_HOST=https://api.aife.economie.gouv.fr/dila/legifrance-beta/lf-engine-app`,
+    `API_HOST=https://api.aife.economie.gouv.fr/dila/legifrance/lf-engine-app`,
     `OAUTH_CLIENT_ID=${result.oauthClientId.trim()}`,
     `OAUTH_CLIENT_SECRET=${result.oauthClientSecret.trim()}`,
     `TOKEN_HOST=https://oauth.aife.economie.gouv.fr`,
